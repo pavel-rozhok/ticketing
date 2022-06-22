a15c0cdd4ff1a99766117b5eb3efcfb8948a3d32
prozhok <prozhok@ascendix.net>
2022-06-22 23:24:48 +0300

    dev

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import app from './app';
 
 const start = async () => {
+  console.log('startion up...');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be define');
   }
